9f8a226b07b0a2f9fca4d35fc20ea8fe0ca91866
Rodrigo Guimarães <rodrigo.silvaguimaraes1992@gmail.com>
2021-06-14 18:10:52 -0300

    subindo dos os arquivos para o repositorio

---
+++ README.md
@@ -0,0 +1,19 @@
+# Jogo de Xadrez Console
+
+## Sobre o sistema
+Esse jogo de xadrez no console, foi desenvolvido com a linguagem C#. Possui todos os mecanismos e regras de um jogo de xadrez tradicional. Para a construção desse programa, foi utilizado todos os conceitos de Orientação a objeto.
+
+## Layout do jogo
+<img src="Tela-Jogo-Xadrez2.PNG" ></img>
+
+Para jogar é bem simples, basta digitar a posição de origem da peça que deseja. Feito isso, o tabuleiro selecionara os campos que esse peça pode se mover. Depois só digitar a posição de destino para mover a peça no jogo.
+Exemplo: Quero mover o cavalo da peça branca que esta com o sigla 'C', basta digitar "g1". Após isso, o tabuleiro selecionara os campos que o cavalo pode mover, no caso da imagem acima só podera mover para duas posições, "f4" ou "h4".
+
+## Conceitos utilizados para desenvolver o jogo
+- Classes abstratas
+- Classes e métodos staticos
+- Herança
+- Polimorfismo
+- Sobrecarga
+- Comunicação de um objeto com outro
+- Tratamento de Exceções
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,52 @@
+﻿namespace tabuleiro
+{
+    abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimento { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor) 
+        {
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtdMovimento = 0;
+        }
+
+        public void incrementarQtdMovimentos() 
+        {
+            qtdMovimento++;
+        }
+
+        public void decrementarQtdMovimentos()
+        {
+            qtdMovimento--;
+        }
+
+        public bool existeMovimentosPossiveis() 
+        {
+            bool[,] mat = movimentosPossiveis();
+            for(int i=0; i<tab.linhas; i++) 
+            { 
+                for(int j=0; j < tab.colunas; j++) 
+                { 
+                    if(mat[i, j]) 
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos) 
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+    }
+}
